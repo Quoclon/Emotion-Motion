55ee651675ea3cd6bb6864f58813f503c107dbb8
Broden D'Angio <Brodendangio@gmail.com>
2021-04-24 00:10:13 -0700

    no message

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(
+            Input.GetAxis("Horizontal") * speed * Time.deltaTime,
+            Input.GetAxis("Vertical") * speed * Time.deltaTime, 0f);
+    }
+}
